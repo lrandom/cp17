9aef2ebb24147c3c8e27860dc5632eb0b7204853
root <luann4099@gmail.com>
2021-04-02 10:36:36 +0700

    ex7

---
+++ exercise7/main.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button');
+button.addEventListener('click',function () {
+    const firstLis = document.querySelectorAll('body ul:nth-child(1) > li');
+    const secondLis = document.querySelectorAll('body ul:nth-child(2) > li');
+    const ul = document.querySelector('body ul:nth-child(3)');
+    for(let i = 0; i<firstLis.length; i++){
+        for(let j = 0; j<secondLis.length; j++){
+            if(parseInt(firstLis[i].innerText)
+                ===parseInt(secondLis[j].innerText)){
+                const newLi = document.createElement('li');
+                newLi.innerText = firstLis[i].innerText;
+                ul.append(newLi);
+            }
+        }
+    }
+
+
+})
