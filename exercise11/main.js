ffee1be7a8a3dc61a3de171fd61edc5544948b3c
root <luann4099@gmail.com>
2021-04-07 09:45:28 +0700

    finish exercise 10

--- exercise11/main.js
+++ exercise11/main.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        content: "Câu hỏi 1: Đông Lào là nước nào",
+        content: "Câu hỏi 1: Đông Lào là nước nào ?",
         answers:[
             "A.Việt Nam",
             "B.Lào",
@@ -20,3 +20,47 @@ const questions = [
         correctAnswer:3
     }
 ]
+
+//câu thứ nhất
+let totalScore = 0;
+const scoreEchCorrectAnswer = 10;
+let questionIndex = 0;
+
+function loadQuestion() {
+    const question = questions[questionIndex];
+    const questionUI = document.querySelector('.wrapper .question');
+    questionUI.innerHTML = question.content;
+    const answersUI = document.querySelector('.wrapper .answers');
+//làm rỗng
+    answersUI.innerHTML = '';
+    const answers = question.answers; //trả về một mảng đáp án
+    const correctAnswer = question.correctAnswer;
+    for (let i = 0; i < answers.length; i++) {
+        let newElem = document.createElement(`div`);
+        newElem.classList.add('answer');
+        newElem.innerHTML = answers[i];
+        newElem.addEventListener('click', function () {
+            if (i == correctAnswer) {
+                alert("Bạn đã trả lời đúng");
+                totalScore += scoreEchCorrectAnswer;
+                const scoreUI = document.querySelector('.score span');
+                scoreUI.innerHTML = totalScore;
+                questionIndex++;
+                if(questionIndex>=questions.length){
+                    alert("Bạn đã hoàn thành bài test");
+                    return;
+                }
+                loadQuestion();
+            } else {
+                alert("Bạn đã trả lời sai");
+                answersUI.innerHTML='';
+
+            }
+        })
+        answersUI.append(newElem);
+    }
+}
+
+
+//tải question lần đầu tiên
+loadQuestion();
