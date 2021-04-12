84c19e17858043e6863fbccf582e653564e09738
root <luann4099@gmail.com>
2021-04-12 08:57:08 +0700

    bt exercise 13

---
+++ exercise13/main.js
@@ -0,0 +1,37 @@
+const arr =  [
+    {
+        brand: 'Huyndai',
+        model:'Santafe'
+    },
+    {
+        brand: 'Huyndai',
+        model:'Sonata'
+    },
+    {
+        brand: 'Vinfast',
+        model:'Lux SA'
+    },
+    {
+        brand: 'Toyota',
+        model:'Camry'
+    },
+    {
+        brand:'Vinfast',
+        model:'Lux A'
+    },
+    {
+        brand:'Toyota',
+        model:'Vios'
+    }
+]
+
+/*
+  const object = {
+     "Huyndai":["Santafe","Sonata"],
+     "Vinfast":["Lux Sa","Lux A"],
+     "Toyota":["Camry","Vios"]
+  }
+* */
+//mang trong js là mảng động
+
+
