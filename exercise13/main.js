57f249c8a7f3820ab63cc3f682cdadbce5e7d66a
root <luann4099@gmail.com>
2021-04-12 10:36:59 +0700

    exercise 12

--- exercise13/main.js
+++ exercise13/main.js
@@ -1,37 +1,52 @@
-const arr =  [
+const arr = [
     {
         brand: 'Huyndai',
         model:'Santafe'
-    },
-    {
+    },{
         brand: 'Huyndai',
         model:'Sonata'
-    },
-    {
+    },{
         brand: 'Vinfast',
         model:'Lux SA'
-    },
-    {
+    },{
         brand: 'Toyota',
         model:'Camry'
-    },
-    {
+    },{
         brand:'Vinfast',
         model:'Lux A'
-    },
-    {
+    },{
         brand:'Toyota',
         model:'Vios'
-    }
-]
-
-/*
-  const object = {
-     "Huyndai":["Santafe","Sonata"],
-     "Vinfast":["Lux Sa","Lux A"],
-     "Toyota":["Camry","Vios"]
-  }
-* */
+    }]
+/* const arr2= [
+     ["Santafe","Sonata"],
+     ["Lux Sa","Lux A"],
+     ["Camry","Vios"]
+  ]*/
 //mang trong js là mảng động
+let group =[];
 
+let index = 0;
+while(arr.length>0) {
+    index++;
+    let temp = [arr[0]];
+    let removeIndex = [0];
+    for (let j = 1; j < arr.length; j++) {
+        if (temp[0].brand == arr[j].brand) {
+            temp.push(arr[j]);
+            removeIndex.push(j);
+        }
+    }
+
+    //console.log("Lần"+index);
+    //console.log(removeIndex);
+    group.push(temp);
+    for (let j = 0; j < removeIndex.length; j++) {
+        arr.splice(removeIndex[j], 1);
+    }
+    console.log(arr);
+}
+
+
+console.log(group);
 
