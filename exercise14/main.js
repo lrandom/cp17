7c1b034476af5cc0ee01ab28464156f4051f8d51
root <luann4099@gmail.com>
2021-04-12 11:33:30 +0700

    exercise 14

---
+++ exercise14/main.js
@@ -0,0 +1,27 @@
+let arr = [
+    {
+        brand: 'Huyndai',
+        model:'Santafe'
+    },{
+        brand: 'Huyndai',
+        model:'Sonata'
+    },{
+        brand: 'Vinfast',
+        model:'Lux SA'
+    },{
+        brand: 'Toyota',
+        model:'Camry'
+    },{
+        brand:'Vinfast',
+        model:'Lux A'
+    },{
+        brand:'Toyota',
+        model:'Vios'
+    }]
+/* const obj= {
+     "Huyndai" :["Santafe","Sonata"],
+     "Vinfast" : ["Lux Sa","Lux A"],
+     "Toyota": ["Camry","Vios"]
+  }*/
+//mang trong js là mảng động
+
